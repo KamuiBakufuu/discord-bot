a90b647412bed645ff25f8941e08fb349fb7bb5a
Leana <mollerleana3@gmail.com>
2024-07-24 17:40:22 +0200

    tried to add buttons (not wokring)

--- commands/game/play.js
+++ commands/game/play.js
@@ -1,10 +1,28 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } = require('@discordjs/builders');
+const { Component } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Play a game!'),
     async execute(interaction) {
-        await interaction.reply('Not yet implemented!');
+
+        const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel('Yes')
+            .setStyle('Success');
+        
+        const cancel = new ButtonBuilder()
+            .setCustomId('cancel')
+            .setLabel('No')
+            .setStyle('Danger');
+
+        const row = new ActionRowBuilder()
+            .addComponents(confirm, cancel);
+
+        await interaction.reply({
+            content: 'Welcome! Would you like to play a game?',
+            Components: [row],
+        });
     },
 };
